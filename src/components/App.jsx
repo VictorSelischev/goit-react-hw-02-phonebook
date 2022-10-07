c4a2c5dcb20a259ac9f9d20bc05fe09140206955
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-10-07 13:21:42 +0300

    Переписываем форму в класс

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,20 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
-
-import { FormPhonbook } from './FormPhonbook/FormPhonbook';
+import FormPhonbook from './FormPhonbook/FormPhonbook';
 import { Section } from './Section/Section';
 import { Contacts } from './Contacts/Contacts';
 
 class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-  };
-
-  handleChange = e => {
-    this.setState({ name: e.target.value });
-  };
-
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    const contact = {};
-    contact.id = nanoid();
-    contact.name = this.state.name;
-    console.log(contact);
-
-    // this.setState(prevState => {
-    //   return { contacts: prevState.contacts.push(contact) };
-    // })
-    
   };
 
   render() {
     const { contacts, name } = this.state;
-    const handleSubmit = this.handleSubmit;
-    const handleChange = this.handleChange;
     return (
       <div
         style={{
@@ -51,7 +29,7 @@ class App extends Component {
         }}
       >
         <Section title={'Phonbook'}>
-          <FormPhonbook handleSubmit={handleSubmit} handleChange={handleChange} name={name} />
+          <FormPhonbook />
         </Section>
         {/* <Section title={'Contacts'}>
           <Contacts contacts={contacts} />
--- src/components/FormPhonbook/FormPhonbook.jsx
+++ src/components/FormPhonbook/FormPhonbook.jsx
@@ -1,22 +1,71 @@
 import css from './FormPhonbook.module.css';
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
 
-export const FormPhonbook = ({handleSubmit, handleChange, name}) => {
-  return (
-    <form className={css.formAddBook}>
-      <label className={css.formAddBook__label}>
-        Name
-        <input
-          className={css.formAddBook__input}
-          type="text"
-          name="name"
-          value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Add contact</button>
-    </form>
-  );
-};
+class FormPhonbook extends Component {
+  state = {
+    name: '',
+  };
+
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const contact = {};
+    contact.id = nanoid();
+    contact.name = this.state.name;
+    console.log(contact);
+
+    this.setState(prevState => {
+      return { contacts: prevState.contacts.push(contact) };
+    });
+
+    console.log(this.state.contacts);
+  };
+
+  render() {
+    return (
+      <form className={css.formAddBook} onSubmit={this.handleSubmit}>
+        <label className={css.formAddBook__label}>
+          Name
+          <input
+            className={css.formAddBook__input}
+            type="text"
+            name="name"
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default FormPhonbook;
+
+// export const FormPhonbook = ({ handleSubmit, handleChange, name }) => {
+//   return (
+//     <form className={css.formAddBook} onSubmit={handleSubmit}>
+//       <label className={css.formAddBook__label}>
+//         Name
+//         <input
+//           className={css.formAddBook__input}
+//           type="text"
+//           name="name"
+//           value={name}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           required
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <button type="submit">Add contact</button>
+//     </form>
+//   );
+// };
